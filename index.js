96780e2203940d7016c5ef5af74473dd736a3ea6
Osvaldo Sanchez <osvaldozurdo7@gmail.com>
2023-06-07 11:48:23 -0600

    instalacion

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const routerApi = require('./routes');
+
+const {logErrors, errorHandler, boomErrorHandler} = require('./middlewares/error.handler');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const whitelist = ['http://localhost:8080', 'http://myapp.co'];
+const options = {
+  origin: (origin, callback) =>{
+    if(whitelist.includes(origin)){
+      callback(null, true);
+    }else{
+      callback(new Error('no permitido'));
+    }
+  }
+}
+
+app.use(cors(options));
+
+app.get('/api/', (req,res) =>{
+  res.send('Hola mi server en express');
+});
+
+
+app.get('a/pi/nueva-ruta', (req,res) =>{
+  res.send('Hola soy una nueva ruta');
+});
+
+routerApi(app);
+
+app.use(errorHandler);
+app.use(boomErrorHandler);
+app.use(logErrors);
+
+
+app.listen(port, () =>{
+  console.log('Mi port' + port);
+});
+
